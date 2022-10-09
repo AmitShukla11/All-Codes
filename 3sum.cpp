67a7798a4cb1af675d9d269808f43d8f2e04c9da
Amit Dhakite <amitdhakite24@gmail.com>
2022-10-09 10:10:14 +0530

    3sum

---
+++ 3sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& v, int t) 
+    {
+        int n = v.size(), res = INT_MAX;
+        sort(begin(v), end(v));
+        for(int i = 0; i < n; i++)
+        {
+            int j = i + 1, k = n - 1;
+            while(j < k)
+            {
+                int curSum = v[j] + v[k];
+                if(res == INT_MAX || abs(curSum + v[i] - t) < abs(res - t))
+                    res = curSum + v[i];
+                
+                if(curSum + v[i] < t)
+                    j++;
+                else
+                    k--;
+            }
+        }
+        return res;
+    }
+};
