bb6338aaa7243db9fc73b8aa22a0bf5f155a044a
syadav-7 <116284227+syadav-7@users.noreply.github.com>
2022-10-20 19:15:50 +0530

    added time.cpp

---
+++ time.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"this code doesnot do anything special";
+    // Just random typing and all
+    // doing time pass
+    cout << "this is for hacktoberfest 2022";
+  
+  return 0;
+}
